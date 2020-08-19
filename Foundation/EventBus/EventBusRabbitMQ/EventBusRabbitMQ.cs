da6ed9807e6db018f58d90cce51d3fef246b4fd1
haristauqir <haristauqir@gmail.com>
2020-08-19 19:11:34 +0100

    Added RabbitMQ Project

--- Foundation/EventBus/EventBusRabbitMQ/EventBusRabbitMQ.cs
+++ Foundation/EventBus/EventBusRabbitMQ/EventBusRabbitMQ.cs
@@ -1,9 +1,20 @@
 using Foundation.EventBus.Abstractions;
+using Foundation.EventBus.Events;
 
 namespace Foundation.EventBusRabbitMQ
 {
-    public class EventBusRabbitMQ : IEventBus
+  public class EventBusRabbitMQ : IEventBus
+  {
+    public void Publish(IntegrationEvent @event)
     {
-        
+      throw new System.NotImplementedException();
     }
+
+    public void Subscribe<T, TH>()
+      where T : IntegrationEvent
+      where TH : IIntegrationEventHandler<T>
+    {
+      throw new System.NotImplementedException();
+    }
+  }
 }
