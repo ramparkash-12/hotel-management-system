fecb76b9e34008a69b6c2186d25e83b80b92f32c
haristauqir <haristauqir@gmail.com>
2020-08-22 16:49:36 +0100

    Identity Domain Classes added

---
+++ Services/Identity/Identity.API/Models/AppUser.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Identity;
+
+namespace Identity.API.Models
+{
+    public class AppUser : IdentityUser
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string PictureUrl { get; set; }
+    }
+}
