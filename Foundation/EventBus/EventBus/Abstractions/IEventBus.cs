9ca75c58211141d6f8505558acd349719609051c
haristauqir <haristauqir@gmail.com>
2020-08-22 13:55:38 +0100

    Implemented RabbitMQ Subscribe Classes

--- Foundation/EventBus/EventBus/Abstractions/IEventBus.cs
+++ Foundation/EventBus/EventBus/Abstractions/IEventBus.cs
@@ -1,13 +1,20 @@
+using Foundation.EventBus.Abstractions;
 using Foundation.EventBus.Events;
 
 namespace Foundation.EventBus.Abstractions
 {
-    public interface IEventBus
-    {
-        void Publish(IntegrationEvent @event);
+  public interface IEventBus
+  {
+    void Publish(IntegrationEvent @event);
 
-        void Subscribe<T, TH>()
-            where T: IntegrationEvent
-            where TH: IIntegrationEventHandler<T>;
-    }
+    void Subscribe<T, TH>()
+        where T : IntegrationEvent
+        where TH : IIntegrationEventHandler<T>;
+
+    void SubscribeDynamic<TH>(string eventName)
+    where TH : IDynamicIntegrationEventHandler;
+
+    void UnsubscribeDynamic<TH>(string eventName)
+        where TH : IDynamicIntegrationEventHandler;
+  }
 }
---
+++ Foundation/EventBus/EventBus/SubscriptionInfo.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using Foundation.EventBus.Abstractions;
+using Foundation.EventBus.Events;
+
+namespace Foundation.EventBus
+{
+  public partial class InMemoryEventBusSubscriptionsManager : IEventBusSubscriptionsManager
+  {
+    public class SubscriptionInfo
+    {
+      public bool IsDynamic { get; }
+      public Type HandlerType { get; }
+
+      private SubscriptionInfo(bool isDynamic, Type handlerType)
+      {
+        IsDynamic = isDynamic;
+        HandlerType = handlerType;
+      }
+
+      public static SubscriptionInfo Dynamic(Type handlerType)
+      {
+        return new SubscriptionInfo(true, handlerType);
+      }
+      public static SubscriptionInfo Typed(Type handlerType)
+      {
+        return new SubscriptionInfo(false, handlerType);
+      }
+    }
+  }
+}
