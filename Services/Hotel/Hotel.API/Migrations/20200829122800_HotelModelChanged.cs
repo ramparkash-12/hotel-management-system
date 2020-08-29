8141c2cd251a6ee2ef7488fafa86a9c022fea7a6
haristauqir <haristauqir@gmail.com>
2020-08-29 13:59:09 +0100

    Hotel service image added to docker file

---
+++ Services/Hotel/Hotel.API/Migrations/20200829122800_HotelModelChanged.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Hotel.API.Migrations
+{
+    public partial class HotelModelChanged : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Address",
+                table: "Hotels",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "City",
+                table: "Hotels",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Country",
+                table: "Hotels",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Address",
+                table: "Hotels");
+
+            migrationBuilder.DropColumn(
+                name: "City",
+                table: "Hotels");
+
+            migrationBuilder.DropColumn(
+                name: "Country",
+                table: "Hotels");
+        }
+    }
+}
--- Services/Hotel/Hotel.API/Model/Hotel.cs
+++ Services/Hotel/Hotel.API/Model/Hotel.cs
@@ -6,6 +6,9 @@ namespace Hotel.API.Model
         public int Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
         public ICollection<Room> Rooms { get; set; }
     }
 }
