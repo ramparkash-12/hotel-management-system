4d313c523c62063b0240916c39ec8219b840be96
haristauqir <haristauqir@gmail.com>
2020-09-04 17:31:47 +0100

    added apigateway as a docker service

--- ApiGateway/ApiGateway/Startup.cs
+++ ApiGateway/ApiGateway/Startup.cs
@@ -39,7 +39,7 @@ namespace ApiGateway
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseHttpsRedirection();
+            //app.UseHttpsRedirection();
 
             app.UseRouting();
 
