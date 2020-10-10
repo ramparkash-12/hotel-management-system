86b2d67c250715dc2076fcd48b401457ef86ed43
haristauqir <haristauqir@gmail.com>
2020-10-10 17:04:21 +0100

    hotel components added

--- Web/WebSPA/ClientApp/src/app/admin/admin-layout.routing.ts
+++ Web/WebSPA/ClientApp/src/app/admin/admin-layout.routing.ts
@@ -3,6 +3,9 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { AdminLayoutComponent } from './admin-layout.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { HotelAddComponent } from './hotel/hotel-add/hotel-add.component';
+import { HotelEditComponent } from './hotel/hotel-edit/hotel-edit.component';
+import { HotelListComponent } from './hotel/hotel-list/hotel-list.component';
 
 const adminRoutes: Routes = [
   {
@@ -12,6 +15,18 @@ const adminRoutes: Routes = [
       {
         path: 'dashboard',
         component: DashboardComponent
+      },
+      {
+        path: 'hotel',
+        component: HotelListComponent
+      },
+      {
+        path: 'hotel/add',
+        component: HotelAddComponent
+      },
+      {
+        path: 'hotel/edit',
+        component: HotelEditComponent
       }
     ]
   }
--- Web/WebSPA/ClientApp/src/app/admin/admin.module.ts
+++ Web/WebSPA/ClientApp/src/app/admin/admin.module.ts
@@ -3,9 +3,21 @@ import { NgModule } from '@angular/core';
 import { AdminLayoutComponent } from './admin-layout.component';
 import { AdminLayoutRouting } from './admin-layout.routing';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { NavMenuComponent } from './nav-menu/nav-menu.component';
+import { SidebarMenuComponent } from './sidebar-menu/sidebar-menu.component';
+import { FooterComponent } from './footer/footer.component';
+import { HotelListComponent } from './hotel/hotel-list/hotel-list.component';
+import { HotelItemComponent } from './hotel/hotel-list/hotel-item/hotel-item.component';
+import { HotelAddComponent } from './hotel/hotel-add/hotel-add.component';
+import { HotelEditComponent } from './hotel/hotel-edit/hotel-edit.component';
 
 @NgModule({
-  declarations: [AdminLayoutComponent, DashboardComponent],
+  declarations: [
+    AdminLayoutComponent,
+    DashboardComponent, NavMenuComponent,
+    SidebarMenuComponent, FooterComponent,
+    HotelListComponent, HotelItemComponent, HotelAddComponent, HotelEditComponent
+  ],
   imports: [AdminLayoutRouting],
   providers: []
 })
