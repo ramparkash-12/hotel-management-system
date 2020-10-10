81449ce5882336632c986f8499652166eade5169
haristauqir <haristauqir@gmail.com>
2020-10-10 11:01:19 +0100

    customer layout home page added

--- Web/WebSPA/ClientApp/src/app/customer/customer.module.ts
+++ Web/WebSPA/ClientApp/src/app/customer/customer.module.ts
@@ -2,9 +2,12 @@ import { NgModule } from '@angular/core';
 import { CustomerLayoutComponent } from './customer-layout.component';
 import { CustomerLayoutRouting } from './customer-layout.routing';
 import { HomeComponent } from './home/home.component';
+import { NavMenuComponent } from './nav-menu/nav-menu.component';
+import { FooterComponent } from './footer/footer.component';
+import { SearchComponent } from './search/search.component';
 
 @NgModule({
-  declarations: [ CustomerLayoutComponent, HomeComponent],
+  declarations: [ CustomerLayoutComponent, HomeComponent, NavMenuComponent, FooterComponent, SearchComponent],
   imports: [CustomerLayoutRouting]
 })
 export class CustomerModule { }
